b5f650e52d8fa829f82cc3b9689d6bd9b9bb780f
Bill Wu <hi@limaois.me>
2020-10-27 04:10:40 -0400

    Add cache feature to service worker

--- amis-deploy/sw.js
+++ amis-deploy/sw.js
@@ -1,2 +1,32 @@
+const CACHE_NAME = 'cache-v1';
+self.addEventListener('install', function() {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+        '/js/deps.js',
+        '/s/revdict-amis-def.txt',
+        '/s/revdict-amis-ex.txt',
+        '/s/stem-words.json',
+        '/s/index.json'
+    ]);
+});
 self.addEventListener('fetch', function(event) {
+    if (event.request.method != 'GET') return;
+
+    event.respondWith(async function() {
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(event.request);
+
+        if (cachedResponse) {
+            event.waitUntil(cache.add(event.request));
+            return cachedResponse;
+        }
+
+        return fetch(event.request).then(function (response) {
+            if (response && response.status === 200) {
+                cache.put(event.target, response.clone());
+            }
+
+            return response;
+        });
+    }());
 });
