997e94facdaee5583e20b233db6ae40c59e87b6e
Sih Sîng-hông薛丞宏 <ihcaoe@gmail.com>
2020-12-18 17:03:38 +0800

    改合成音檔API

--- view.js
+++ view.js
@@ -220,7 +220,7 @@
           className: 'youyin'
         }, youyin));
       }
-      mp3 = `https://服務.意傳.台灣/%E6%96%87%E6%9C%AC%E7%9B%B4%E6%8E%A5%E5%90%88%E6%88%90?%E6%9F%A5%E8%A9%A2%E8%85%94%E5%8F%A3=Pangcah&%E6%9F%A5%E8%A9%A2%E8%AA%9E%E5%8F%A5=${t}`;
+      mp3 = `https://hts.ithuan.tw/%E6%96%87%E6%9C%AC%E7%9B%B4%E6%8E%A5%E5%90%88%E6%88%90?%E6%9F%A5%E8%A9%A2%E8%85%94%E5%8F%A3=Pangcah&%E6%9F%A5%E8%A9%A2%E8%AA%9E%E5%8F%A5=${t}`;
       if (mp3) {
         list = list.concat(i({
           itemType: 'http://schema.org/AudioObject',
